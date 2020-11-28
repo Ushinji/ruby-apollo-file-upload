92900897628d15f3d0f52999367e61412ab44071
Ushinji <siguma971@gmail.com>
2020-11-29 06:36:32 +0900

    Add ImageUploader to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   validates :display_name, presence: true
   validates :image, presence: true
 end
