cd6265d3250481916969ff4e8c93b7326bb596e2
Ushinji <siguma971@gmail.com>
2020-11-29 06:08:17 +0900

    Setup Graphql

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  post "/graphql", to: "graphql#execute"
   get "/health_check", to: "health_check#index"
   get "/", to: "root#index"
 end
