d8d462d8faad83703c8d54c52a5994c74f10d81a
Ushinji <siguma971@gmail.com>
2020-12-01 21:47:42 +0900

    Add csrf token to apollo client header

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '@/App';
 
+declare global {
+  interface Window {
+    csrfToken: string;
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/modules/apolloClient.ts
+++ src/modules/apolloClient.ts
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const apolloClient = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    'X-CSRF-Token': window.csrfToken,
+  },
 });
 
 export default apolloClient;
