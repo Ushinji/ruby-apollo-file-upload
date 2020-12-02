ea3b03fe3c3a6afaa215471f9c933d463705a1aa
Ushinji <siguma971@gmail.com>
2020-12-02 20:48:25 +0900

    Setup apollo-upload-client

--- src/modules/apolloClient.ts
+++ src/modules/apolloClient.ts
@@ -1,11 +1,15 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
 
 const apolloClient = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
-  headers: {
-    'X-CSRF-Token': window.csrfToken,
-  },
+  link: createUploadLink({
+    uri: '/graphql',
+    headers: {
+      'X-CSRF-Token': window.csrfToken,
+    },
+  }),
 });
 
 export default apolloClient;
